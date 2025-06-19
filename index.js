ccdcb52331c23b62514098b14791513563f7ad50
Carlos-SaulPM <carlossaulpazmaldonado@gmail.com>
2025-06-18 21:15:36 -0600

    Agregando cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const {
@@ -8,6 +9,7 @@ const {
   actualizar,
 } = require("./pelicula.repositorio");
 
+app.use(cors());
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
 
